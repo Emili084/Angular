4958b9d59abc7ed553ae3dabac07bf34a11b2da8
Emili084 <128594426+Emili084@users.noreply.github.com>
2023-05-15 10:25:10 +0200

    update user-profil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from "./app.component";
 import { BlockComponent } from './block/block.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
 
 @NgModule({
-  declarations: [AppComponent, BlockComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, BlockComponent, UserProfilComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/user-profil/user-profil.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profil',
+  templateUrl: './user-profil.component.html',
+  styleUrls: ['./user-profil.component.css']
+})
+export class UserProfilComponent {
+  user = {
+    name : 'Doe',
+    firstName : 'John',
+    age : 25,
+    quote : '',
+    photo : 'https://randomuser.me/api/portraits/lego/2.jpg',
+    isAgeHidden: false
+  };
+
+  toggleAgeVisibility(): void {
+    this.user.isAgeHidden = !this.user.isAgeHidden
+  };
+}
