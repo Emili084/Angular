13754e0ff3c9ba573a3f4c3ca30660b805fdb5d2
Emili084 <128594426+Emili084@users.noreply.github.com>
2023-05-15 10:57:25 +0200

    update user-profil

--- src/app/user-profil/user-profil.component.ts
+++ src/app/user-profil/user-profil.component.ts
@@ -1,21 +1,50 @@
 import { Component } from '@angular/core';
 
+export class User {
+  name: string;
+  firstName: string;
+  age: number;
+  quote: string;
+  photo: string;
+  isAgeHidden: boolean;
+
+  constructor(
+    name: string,
+    firstName: string,
+    age: number,
+    quote: string,
+    photo: string,
+    isAgeHidden: boolean
+  ) {
+    this.name = name;
+    this.firstName = firstName;
+    this.age = age;
+    this.quote = quote;
+    this.photo = photo;
+    this.isAgeHidden = isAgeHidden;
+  }
+}
+
 @Component({
   selector: 'app-user-profil',
   templateUrl: './user-profil.component.html',
   styleUrls: ['./user-profil.component.css']
 })
 export class UserProfilComponent {
-  user = {
-    name : 'Doe',
-    firstName : 'John',
-    age : 25,
-    quote : '',
-    photo : 'https://randomuser.me/api/portraits/lego/2.jpg',
-    isAgeHidden: false
-  };
+  user: User;
 
-  toggleAgeVisibility(): void {
-    this.user.isAgeHidden = !this.user.isAgeHidden
-  };
+  constructor() {
+    this.user = new User(
+      'Doe',
+      'John',
+      25,
+      '',
+      'https://randomuser.me/api/portraits/lego/2.jpg',
+      false
+    );
+  }
+
+  ageVisibility(): void {
+    this.user.isAgeHidden = !this.user.isAgeHidden;
+  }
 }
