b11238b5d53421fad608b0595bc3e7d610048f2f
Emili084 <128594426+Emili084@users.noreply.github.com>
2023-05-17 14:27:10 +0200

    update menu and sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilComponent } from './user-profil/user-profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'user', component: UserProfilComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './menu.component';
 
 import { FormsModule } from '@angular/forms';
 
@@ -7,10 +9,12 @@ import { AppComponent } from "./app.component";
 import { BlockComponent } from './block/block.component';
 import { UserProfilComponent } from './user-profil/user-profil.component';
 import { DirectivesComponent } from './directives/directives.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent, BlockComponent, UserProfilComponent, DirectivesComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, MenuComponent, BlockComponent, UserProfilComponent, DirectivesComponent, CatalogComponent, SignUpComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
